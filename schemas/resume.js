1bda3213e1483de766ca97d5ab45072822cf4046
patsanjuan <patrick@sudozi.com>
2024-01-30 18:43:01 -0800

    add skills and experience

--- schemas/resume.js
+++ schemas/resume.js
@@ -17,6 +17,51 @@ export default {
       name: 'email',
       type: 'string',
       title: 'Email'
+    },
+    {
+      name: 'skills',
+      type: 'array',
+      title: 'Skills',
+      of: [{ type: 'string' }],
+      validation: Rule => Rule.unique()
+    },
+    {
+      name: 'experiences',
+      type: 'array',
+      title: 'Experience',
+      of: [
+        {
+          name: 'experience',
+          type: 'document',
+          fields: [
+            {
+              name: 'title',
+              type: 'string',
+              title: 'Title'
+            },
+            {
+              name: 'company',
+              type: 'string',
+              title: 'Company'
+            },
+            {
+              name: 'location',
+              type: 'string',
+              title: 'Location'
+            },
+            {
+              name: 'startDate',
+              type: 'date',
+              title: 'Start Date'
+            },
+            {
+              name: 'endDate',
+              type: 'date',
+              title: 'End Date'
+            }
+          ]
+        }
+      ]
     }
   ]
 }
