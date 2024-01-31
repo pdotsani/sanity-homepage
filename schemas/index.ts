22c4240a2e50e2188b337999dd919f9ec0287f49
patsanjuan <patrick@sudozi.com>
2024-01-30 18:19:44 -0800

    initial resume schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import resume from './resume';
+
+export const schemaTypes = [resume];
---
+++ schemas/resume.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'resume',
+  type: 'document',
+  title: 'Resume',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'city',
+      type: 'string',
+      title: 'City'
+    },
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email'
+    }
+  ]
+}
